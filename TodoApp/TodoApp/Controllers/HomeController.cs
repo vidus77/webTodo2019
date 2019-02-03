5b70e39aa9107672bc0778d9840b2e170153c878
Zoltan Vida <vidus77@gmail.com>
2019-02-03 19:54:19 +0300

    #2: Sajar Action es sajat View segitssegevel listat generaltunk

---
+++ TodoApp/TodoApp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TodoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult TodoList()
+        {
+            //bevásárló lista adatai:
+            var lista = new List<string>();
+
+            lista.Add("Só");
+            lista.Add("Cukor");
+            lista.Add("Spagetti");
+            lista.Add("Marhahús");
+            lista.Add("Paradicsom");
+
+            ViewBag.Lista = (lista);
+           return View();
+        }
+
+    }
+}
